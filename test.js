4ba7918bfa87fc59e9d6ff1baa41f68ecbdae040
Liam Moore <39091558+ZyC0R3@users.noreply.github.com>
2019-09-12 02:05:32 +0100

    Update test.js

--- test.js
+++ test.js
@@ -8,8 +8,32 @@ test.beforeEach(() => {
     config.clear();
 });
 
-test('translate from en to dutch - Hello', async t => {
-    const res = await translate('hello', {from: 'en', to: 'nl'});
+test('translate from en to ps (Pashto) - Hello', async t => {
+    const res = await translate('hello', {from: 'en', to: 'ps'});
 
-    t.is(res.text, 'Hallo');
+    t.is(res.text, 'سلام');
+});
+
+test('translate from en to sv (Swedish) - Hello', async t => {
+    const res = await translate('hello', {from: 'en', to: 'sv'});
+
+    t.is(res.text, 'Hej');
+});
+
+test('translate from en to sq (Albanian) - Hello', async t => {
+    const res = await translate('hello', {from: 'en', to: 'sq'});
+
+    t.is(res.text, 'Përshëndetje');
+});
+
+test('translate from en to ar (Arabic) - Hello', async t => {
+    const res = await translate('hello', {from: 'en', to: 'ar'});
+
+    t.is(res.text, 'مرحبا');
+});
+
+test('translate from en to fr (French) - Hello', async t => {
+    const res = await translate('hello', {from: 'en', to: 'fr'});
+
+    t.is(res.text, 'Bonjour');
 });
